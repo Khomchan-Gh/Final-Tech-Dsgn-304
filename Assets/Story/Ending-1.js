b5cffda041ebe79c1e65b289ced991b4330add0d
Khomchan-Gh <khomchan9@gmail.com>
2023-05-23 23:50:15 +0900

    Final Version for Tech Dsgn Exp

--- Assets/Story/Ending-1.js
+++ Assets/Story/Ending-1.js
@@ -1,16 +1,10 @@
 function ending1() {
-
+  
   background(0);
   textAlign(CENTER);
   fill(211);
-  text("Ending#1",width/2,height/2)
-  
-    //reset character values for a new game 
-    isPlayerTurn = true;
-    cecilia.startOver();
-    cecilia.damageDealt = 0;
-    ue.hp = ue.maxhp;
-    eyeCount = eyeMinCount;
-    turnPassed = resetTurn;
+  textSize(32);
+  textFont(novecentoBold);
+  text("Thank you for playing, Story will be updated later ..Maybe :P",width/2,height/2)
   
   }
